56b533e523ffae4faea9394436a1c7ef0d4e51ee
belli49 <pedrokomessu@gmail.com>
2023-02-22 19:48:12 -0300

    Added basic skeleton

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
@@ -7,16 +7,24 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            本日はお忙しいところお越しいただき誠にありがとうございました。
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+            私たちにとって特別なこの日は皆さんのおかげです。
+        </p>
+        <p>
+            素晴らしく忘れられない一日になりました。
+        </p>
+        <p>
+            これからも私たちをどうぞ宜しくお願い致します。
+        </p>
+        <p/>
+        <p>
+            Agradecemos por viver esse momento com a gente.
+        </p>
+        <p>
+            Nosso dia não seria tão especial sem vocês.
+        </p>
       </header>
     </div>
   );
