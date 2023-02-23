df4f3736686f5f13bf25102c88c80b38fcf5948f
belli49 <pedrokomessu@gmail.com>
2023-02-22 22:11:44 -0300

    Added background, dividers and changed Portuguese font

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import logo from './logo.jpg';
 import './App.css';
+import divisor from './divisor.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={divisor} className="Divisor" />
+        <p className="Japanese-text">
             本日はお忙しいところお越しいただき誠にありがとうございました。
         </p>
-        <p>
+        <p className="Japanese-text">
             私たちにとって特別なこの日は皆さんのおかげです。
         </p>
-        <p>
+        <p className="Japanese-text">
             素晴らしく忘れられない一日になりました。
         </p>
-        <p>
+        <p className="Japanese-text">
             これからも私たちをどうぞ宜しくお願い致します。
         </p>
         <p/>
-        <p>
-            Agradecemos por viver esse momento com a gente.
+        <p className="Portuguese-text">
+            AGRADECEMOS POR VIVER ESSE MOMENTO COM A GENTE.
         </p>
-        <p>
-            Nosso dia não seria tão especial sem vocês.
+        <p className="Portuguese-text">
+            NOSSO DIA NÃO SERIA TÃO ESPECIAL SEM VOCÊS.
         </p>
         <p/>
-        <p>
-            Mari & Haroldo
+        <img src={divisor} className="Divisor" />
+        <p className="Portuguese-text">
+            MARI & HAROLDO
         </p>
       </header>
     </div>
